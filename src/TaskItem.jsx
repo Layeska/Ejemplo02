d74fc4e97db629350fb272ec7a2a5c6874a52901
layeska <lisa13suarez@gmail.com>
2023-10-31 20:09:49 -0600

    Editando el item

--- src/TaskItem.jsx
+++ src/TaskItem.jsx
@@ -1,18 +1,18 @@
 import useTaskStore from "./store/useTaskStore";
 
 // Componente para mostrar una tarea y permitir marcarla como completada
-const TaskItem = ({task}) => {
+const TaskItem = (task) => {
   const { toggleTask } = useTaskStore();
-  console.log(task)
+  console.log(task.task)
 
   return (
     <div style={{ marginBottom: '10px' }}>
       <input
         type="checkbox"
-        checked={task.completed}
-        onChange={() => toggleTask(task.id)}
+        checked={task.task.completed}
+        onChange={() => toggleTask(task.task.id)}
       />
-      <span style={{ marginLeft: '10px' }}>{task.text}</span>
+      <span style={{ marginLeft: '10px' }}>{task.task.text}</span>
     </div>
   );
 };
